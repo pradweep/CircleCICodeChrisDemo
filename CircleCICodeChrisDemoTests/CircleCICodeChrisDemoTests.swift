121ec65049de2bd00c157ef5a192547885267421
pradeepiOS <pradeep.iostech@gmail.com>
2021-03-05 10:14:49 +0530

    Added trivial test case

--- CircleCICodeChrisDemoTests/CircleCICodeChrisDemoTests.swift
+++ CircleCICodeChrisDemoTests/CircleCICodeChrisDemoTests.swift
@@ -20,15 +20,9 @@ class CircleCICodeChrisDemoTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let i = 0
+        
+        XCTAssert(i == 0, "I supposed to be zero")
     }
 
 }
